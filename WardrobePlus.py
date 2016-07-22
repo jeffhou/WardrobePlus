603df1bfe8b91d3f1c3df0b12c6d76349323d67f
Jeff <j.king.hou@gmail.com>
2016-07-22 06:31:29 +0000

    fixed spacing inconsistency in WardrobePlus.py

--- WardrobePlus.py
+++ WardrobePlus.py
@@ -95,45 +95,45 @@ def closeDB(error):
     g.sqlite_db.close()
 
 def getClothes():
-    clothes = getClothesDB()
-    clothesList = []
-    for i in clothes:
-        clothName = str(i[1])
-        clothTags = getTagsByClothId(int(i[0])) #TODO need to recast/retype this variable
-        clothInWardrobe = bool(i[2])
-        clothGuid = int(i[0])
-        clothesList.append(Clothing(clothName, clothTags, clothInWardrobe, clothGuid))
-    return clothesList
+  clothes = getClothesDB()
+  clothesList = []
+  for i in clothes:
+    clothName = str(i[1])
+    clothTags = getTagsByClothId(int(i[0])) #TODO need to recast/retype this variable
+    clothInWardrobe = bool(i[2])
+    clothGuid = int(i[0])
+    clothesList.append(Clothing(clothName, clothTags, clothInWardrobe, clothGuid))
+  return clothesList
 
 def getClothing(guid):
-    for i in getClothes():
-        if guid == i.guid:
-            return i
+  for i in getClothes():
+    if guid == i.guid:
+      return i
 
 class Clothing:
-    def __init__(self, name, tags=[], inWardrobe=True, guid=-1):
-        self.name = str(name)
-        self.guid = guid
-        self.tags = tags
-        self.inWardrobe = inWardrobe #TODO: update
-    def isInWardrobe(self):
-        return self.inWardrobe
-    def checkin(self):
-        self.inWardrobe = True
-        updateClothStatus(self.guid, 1) #should update after any change
-    def checkout(self):
-        self.inWardrobe = False
-        updateClothStatus(self.guid, 0) #should update after any change
-    def __str__(self):
-        return "[Clothing]" + str({"name": self.name, "guid": self.guid,
-            "tags": self.tags, "inWardrobe": self.inWardrobe})
+  def __init__(self, name, tags=[], inWardrobe=True, guid=-1):
+    self.name = str(name)
+    self.guid = guid
+    self.tags = tags
+    self.inWardrobe = inWardrobe
+  def isInWardrobe(self):
+    return self.inWardrobe
+  def checkin(self):
+    self.inWardrobe = True
+    updateClothStatus(self.guid, 1)
+  def checkout(self):
+    self.inWardrobe = False
+    updateClothStatus(self.guid, 0)
+  def __str__(self):
+    return "[Clothing]" + str({"name": self.name, "guid": self.guid,
+        "tags": self.tags, "inWardrobe": self.inWardrobe})
 
 @app.route('/')
 def index():
-    createTable("Clothes", [["Name", "TEXT UNIQUE"], ["InWardrobe", "SMALLINT DEFAULT 1"]])
-    createTable("Tags", [["Name", "TEXT UNIQUE"]])
-    createTable("ClothesTagsAssociations", [["ClothId", "INTEGER"], ["TagId", "INTEGER"], ["UNIQUE(ClothId, TagId)", "ON CONFLICT IGNORE"]])
-    return render_template('landing_menu.html')
+  createTable("Clothes", [["Name", "TEXT UNIQUE"], ["InWardrobe", "SMALLINT DEFAULT 1"]])
+  createTable("Tags", [["Name", "TEXT UNIQUE"]])
+  createTable("ClothesTagsAssociations", [["ClothId", "INTEGER"], ["TagId", "INTEGER"], ["UNIQUE(ClothId, TagId)", "ON CONFLICT IGNORE"]])
+  return render_template('landing_menu.html')
 
 @app.route('/edit_wardrobe', methods=['GET'])
 def edit_wardrobe():
@@ -167,60 +167,59 @@ def edit_wardrobe():
 
 @app.route('/begin_session')
 def begin_session():
-    return redirect(url_for("sessionCheckout"))
+  return redirect(url_for("sessionCheckout"))
 
 @app.route('/new_clothing', methods=['POST'])
 def new_clothing():
-    clothing_name = request.form['name']
-    clothing_tags = filter(
-            lambda x: len(x) > 0, str(request.form['tags']).split(","))
-    newClothGuid = addCloth(clothing_name)#todo show status if cloth already exists
-    for i in clothing_tags:
-        tagCloth(newClothGuid, i)
-    return redirect(url_for("edit_wardrobe"))
+  clothing_name = request.form['name']
+  clothing_tags = filter(lambda x: len(x) > 0, str(request.form['tags']).split(","))
+  newClothGuid = addCloth(clothing_name)#todo show status if cloth already exists
+  for i in clothing_tags:
+    tagCloth(newClothGuid, i)
+  return redirect(url_for("edit_wardrobe"))
 
 @app.route('/remove_clothing', methods=['GET', 'POST'])
 def remove_clothing():
-    return render_template('delete_clothing.html', clothes=getClothes())
+  return render_template('delete_clothing.html', clothes=getClothes())
 
 @app.route('/delete_cloth/<int:id>')
 def delete_cloth(id):
-    delCloth(id)
-    return redirect(url_for("edit_wardrobe"))
+  delCloth(id)
+  return redirect(url_for("edit_wardrobe"))
 
 def delete_clothing():
-    clothGuid = int(request.form.get('comp_select'))
-    delCloth(clothGuid)
-    return redirect(url_for("edit_wardrobe"))
+  clothGuid = int(request.form.get('comp_select'))
+  delCloth(clothGuid)
+  return redirect(url_for("edit_wardrobe"))
 
 @app.route('/add_sample_set')
 def add_sample_set():
-    colors = ["Coral", "Black", "White", "Pink", "Blue", "Canary"]
-    styles = ["Shirt", "Pants", "OCBD", "Jacket", "Tie", "Hat"]
-    samples = []
-    for i in colors:
-      for j in styles:
-        samples.append(i + " " + j)
+  colors = ["Coral", "Black", "White", "Pink", "Blue", "Canary"]
+  styles = ["Shirt", "Pants", "OCBD", "Jacket", "Tie", "Hat"]
+  samples = []
+  for i in colors:
+    for j in styles:
+      samples.append(i + " " + j)
 
-    for i in samples:
-      guid = addCloth(i)
-      tagCloth(guid, i.split()[0])
-      tagCloth(guid, i.split()[1])
-    return redirect(url_for("edit_wardrobe"))
+  for i in samples:
+    guid = addCloth(i)
+    tagCloth(guid, i.split()[0])
+    tagCloth(guid, i.split()[1])
+  return redirect(url_for("edit_wardrobe"))
 
 @app.route('/session#checkout')
 def sessionCheckout():
-    if 'clothingGUID' in request.args:
-        clothingGUID = int(request.args['clothingGUID'])
-        getClothing(clothingGUID).checkout()
-    return render_template('session.html', clothes=getClothes())
+  if 'clothingGUID' in request.args:
+    clothingGUID = int(request.args['clothingGUID'])
+    getClothing(clothingGUID).checkout()
+  return render_template('session.html', clothes=getClothes())
 
 @app.route('/session#return')
 def sessionReturn():
-    if 'clothingGUID' in request.args:
-        clothingGUID = int(request.args['clothingGUID'])
-        getClothing(clothingGUID).checkin()
-    return render_template('session.html', clothes=getClothes())
+  if 'clothingGUID' in request.args:
+    clothingGUID = int(request.args['clothingGUID'])
+    getClothing(clothingGUID).checkin()
+  return render_template('session.html', clothes=getClothes())
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', debug=True, port=8080)
+  app.run('0.0.0.0', debug=True, port=8080)
